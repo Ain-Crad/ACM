611d062192f77254e3ab7c4873b67ff7745b6ee7
Aincrad <851056505@qq.com>
2018-09-22 14:22:15 +0800

    ...

--- Uva/Uva548.cpp
+++ Uva/Uva548.cpp
@@ -17,55 +17,55 @@ int n;
 int best, best_sum;
 
 bool read(int* s){
-    if(!getline(cin, line)) return false;
-    stringstream ss(line);
-    n = 0;
-    int x;
-    while(ss >> x){
-        s[n++] = x;
-    }
-    return true;
+	if(!getline(cin, line)) return false;
+	stringstream ss(line);
+	n = 0;
+	int x;
+	while(ss >> x){
+		s[n++] = x;
+	}
+	return true;
 }
 
 int build(int l1, int r1, int l2, int r2){
-    if(l1 > r1) return 0;
-    int v = post_order[r2];
-    int pos = 0;
-    while(in_order[pos] != v) pos++;
-    int len = pos - l1;
-    lch[v] = build(l1, l1 + len - 1, l2, l2 + len - 1);
-    rch[v] = build(l1 + len + 1, r1, l2 + len, r2 - 1);
-    return v;
+	if(l1 > r1) return 0;
+	int v = post_order[r2];
+	int pos = 0;
+	while(in_order[pos] != v) pos++;
+	int len = pos - l1;
+	lch[v] = build(l1, l1 + len - 1, l2, l2 + len - 1);
+	rch[v] = build(l1 + len + 1, r1, l2 + len, r2 - 1);
+	return v;
 }
 
 void dfs(int v, int sum){
-    sum += v;
-    if(!lch[v] && !rch[v]){
-        if(sum < best_sum || (sum == best_sum && v < best)){
-            best_sum = sum;
-            best = v;
-        }
-        return;
-    }
-    if(lch[v]) dfs(lch[v], sum);
-    if(rch[v]) dfs(rch[v], sum);
+	sum += v;
+	if(!lch[v] && !rch[v]){
+		if(sum < best_sum || (sum == best_sum && v < best)){
+			best_sum = sum;
+			best = v;
+		}
+		return;
+	}
+	if(lch[v]) dfs(lch[v], sum);
+	if(rch[v]) dfs(rch[v], sum);
 }
 
 int main(){
-    //ios::sync_with_stdio(false);
-    //cin.tie(0);
-    //cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("in.txt", "r", stdin);
-    #endif
-    
-    while(read(in_order)){
-        read(post_order);
-        int root = build(0, n - 1, 0, n - 1);
-        best_sum = 1e9;
-        dfs(root, 0);
-        cout << best << endl;
-    }
-    
-    return 0;
+	//ios::sync_with_stdio(false);
+	//cin.tie(0);
+	//cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("in.txt", "r", stdin);
+	#endif
+	
+	while(read(in_order)){
+		read(post_order);
+		int root = build(0, n - 1, 0, n - 1);
+		best_sum = 1e9;
+		dfs(root, 0);
+		cout << best << endl;
+	}
+	
+	return 0;
 }
